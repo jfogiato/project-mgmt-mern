7e7a0f3da24ebd50bf2916a8df52ee980a6bcd03
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-03 11:32:54 -0400

    Add: schemas for both Client and Project
    Add: db connection file via Mongoose
    
    Add: MongoDB URI env variable

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const { projects, clients } = require('../sampleData.js');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList
+} = require('graphql');
 
 const ClientType = new GraphQLObjectType({
   name: 'Client',
@@ -12,14 +18,49 @@ const ClientType = new GraphQLObjectType({
   })
 });
 
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    client: { 
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find(client => client.id === parent.clientId);
+      }
+    },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  })
+});
+
 const RootQuery = new GraphQLObjectType ({
   name: 'RootQueryType',
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      }
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return clients.find(client => client.id === args.id);
+      }
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args) {
+        return projects.find(project => project.id === args.id);
       }
     }
   }

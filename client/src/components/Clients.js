fd065e74322c829709cb4381707bb15b8ce50f5a
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-05 12:52:09 -0400

    Fix: cors issue in server
    Add: Clients component buildout

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
 
 export default function Clients() {
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Something went wrong: {error.message}</p>
+
   return (
-    <div>Clients</div>
+    <>
+      {!loading && !error && <h1>Clients</h1>}
+    </>
   )
 }

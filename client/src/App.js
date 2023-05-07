bddb3153dda670144159986539983d88b6e7ded1
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-07 14:49:10 -0400

    Update: Apollo cache object to fix error when deleting client
    Add: clientMutations file & deleteClient GQL query

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,28 @@ import Header from "./components/Header";
 import Clients from "./components/Clients";
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          } 
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          } 
+        }
+      }
+    }
+  }
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
